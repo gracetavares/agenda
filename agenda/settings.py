907c6a1912005d3a486024d2dc620f5c62ed44ae
Grace Tavares <grace.biologia@gmail.com>
2021-09-17 21:39:33 -0300

    Adicionado retorno de lista por Jason, retorno de Http404 e configurado timezone para pt-br

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,10 +25,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yb8@bo+nh-p2o%+5tv=&q&t%z5lx1p3f266j&2#mz+xjpwkfjy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+"""
+Alterado o DEBUG e o ALLOWED_HOSTS para não 
+aparecer o debug de desenvolvimento 
+para o usuário.
+"""
 
+#DEBUG = True
+DEBUG = False
+
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -106,15 +114,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
